b93dbcf5b871ef7f3039e6bd3b9f92956760f7f4
aszteroid <azzahrasamputri@gmail.com>
2026-06-19 10:26:22 +0700

    fix: defined app variable before invoking useStoragePath

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,15 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-if (isset($_SERVER['VERCEL_DIR'])) {
-    $compiledPath = '/tmp/storage/framework/views';
-    if (!is_dir($compiledPath)) {
-        mkdir($compiledPath, 0755, true);
-    }
-    $app->useStoragePath('/tmp/storage');
-}
-
-return Application::configure(basePath: dirname(__DIR__))
+// 1. Buat dulu instance aplikasinya dan tampung ke variabel $app
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -24,3 +17,15 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// 2. Baru jalankan kondisi Vercel menggunakan variabel $app yang sudah ada
+if (isset($_SERVER['VERCEL_DIR'])) {
+    $compiledPath = '/tmp/storage/framework/views';
+    if (!is_dir($compiledPath)) {
+        mkdir($compiledPath, 0755, true);
+    }
+    $app->useStoragePath('/tmp/storage');
+}
+
+// 3. Kembalikan variabel $app ke sistem Laravel
+return $app;
